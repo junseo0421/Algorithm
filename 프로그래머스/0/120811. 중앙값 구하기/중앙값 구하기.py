637ca017314c87a6b2a14fba68610c721e14d93e
junseo0421 <jmdmk9st@naver.com>
2025-05-22 15:54:51 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.01 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.py
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.py
@@ -1,6 +1,4 @@
 def solution(array):
     array.sort()
     
-    length = len(array)
-    
-    return array[length // 2] if length % 2 == 1 else array[length // 2 - 1]
+    return array[len(array) // 2]
