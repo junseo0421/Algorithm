22266be6125bf4c8f462df75df55cf24d74e73b3
junseo0421 <jmdmk9st@naver.com>
2025-05-22 18:29:51 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.03 ms, Memory: 9.46 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
@@ -1,6 +1,2 @@
 def solution(my_string):
-    number_list = [int(i) for i in my_string if i.isdigit()]
-    
-    number_list.sort()
-    
-    return number_list
+    return sorted([int(i) for i in my_string if i.isdigit()])
