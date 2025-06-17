c2f1e39f093362b725e1b526792347dd4f95e66f
junseo0421 <jmdmk9st@naver.com>
2025-06-17 14:57:02 +0900

    [level 0] Title: 특이한 정렬, Time: 0.05 ms, Memory: 9.04 MB -BaekjoonHub

--- 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.py
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.py
@@ -1,8 +1,2 @@
 def solution(numlist, n):
-    sorted_num = sorted(numlist)
-    abs_list = [abs(i-n) for i in sorted_num]
-    num_dict = dict(zip(sorted_num, abs_list))
-    
-    result = sorted(num_dict.items(), key= lambda x: (x[1], -x[0]))
-        
-    return [i[0] for i in result]
+    return sorted(numlist, key= lambda x: (abs(x-n), -x))
