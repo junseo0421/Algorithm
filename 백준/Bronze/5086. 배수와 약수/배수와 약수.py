46e3d9833b05d511e3007a07ea9f21ccd73ed22b
junseo0421 <jmdmk9st@naver.com>
2025-10-01 12:04:22 +0900

    [Bronze III] Title: 배수와 약수, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.py
@@ -0,0 +1,14 @@
+import sys
+
+while True:
+    A, B = map(int, sys.stdin.readline().split())
+    
+    if A == 0 and B == 0:
+        break
+
+    if A % B == 0:
+        print("multiple")
+    elif B % A == 0:
+        print("factor")
+    else:
+        print("neither")
