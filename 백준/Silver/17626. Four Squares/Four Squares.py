2ff35ef2cdf3c3a27f7834117a64458cf0fde21c
Junseo Kim <jmdmk9st@naver.com>
2025-12-02 17:14:25 +0900

    [Silver III] Title: Four Squares, Time: 124 ms, Memory: 109900 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.py
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -4,21 +4,12 @@ input = sys.stdin.readline
 N = int(input())
 dp = [0] * (N+1)
 
-k = 1
-while k*k <= N:
-    dp[k*k] = 1
-    k += 1
-
 for i in range(1, N+1):
-    if dp[i] != 0:  # 완전 제곱수 스킵
-        continue
-
+    dp[i] = i  # 최악의 경우로 초기화 (1로만 합이 이루어짐)
     j = 1
-    while j*j < i:
-        if dp[i] == 0:
-            dp[i] = dp[j*j] + dp[i - j*j]
-        else:
-            dp[i] = min(dp[i], dp[j*j] + dp[i - j*j])
+    
+    while j*j <= i:
+        dp[i] = min(dp[i], dp[i - j*j] + 1)
         j += 1
 
 sys.stdout.write(str(dp[N]))
