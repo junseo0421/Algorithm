52a4161790eee489e7ac10003ae2e500ead2ae3f
Junseo Kim <jmdmk9st@naver.com>
2025-12-12 20:14:15 +0900

    [Silver I] Title: IOIOI, Time: 340 ms, Memory: 34368 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,3 +1,5 @@
+# 출처 : https://velog.io/@hamsangjin/%EB%B0%B1%EC%A4%80-5525%EB%B2%88-IOIOI-%ED%8C%8C%EC%9D%B4%EC%8D%AC
+
 import sys
 input = sys.stdin.readline
 
@@ -5,13 +7,20 @@ N = int(input())
 M = int(input())
 S = input().strip()
 
-string = 'IO' * N + 'I'
+P = 'IOI'
 
-cnt = 0
-string_len = 2*N+1
+cnt = i = answer = 0
 
-for i in range(M - string_len + 1):
-    if string == S[i:i+string_len]:
-        cnt += 1
+while i < (M - 1):
+  if S[i : i+3] == P:
+    i += 2
+    cnt += 1
+    if cnt == N:
+      answer += 1
+      cnt -= 1
 
-sys.stdout.write(str(cnt))
+  else:
+    i += 1
+    cnt = 0
+
+print(answer)
