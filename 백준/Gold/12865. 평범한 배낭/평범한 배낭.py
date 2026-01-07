2c53c3ce734ae2eb8f5b1f930bdcd39bf31b792b
Junseo Kim <jmdmk9st@naver.com>
2026-01-07 14:27:42 +0900

    [Gold V] Title: 평범한 배낭, Time: 2460 ms, Memory: 36264 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+# 경우의 수는 물건을 넣는다/안 넣는다
+# dp[w] = 무게 한도 w에서 가능한 최대 가치
+
+N, K = map(int, input().split())
+
+dp = [0] * (K+1)
+
+for _ in range(N):
+    W, V = map(int, input().split())
+    for w in range(K, W-1, -1):
+        dp[w] = max(dp[w], dp[w-W] + V)
+        
+sys.stdout.write(str(dp[K]))
