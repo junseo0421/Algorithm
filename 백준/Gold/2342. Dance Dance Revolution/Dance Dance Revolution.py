f92f1d3628ffe463e79e3146015d1f84e73fcbae
Junseo Kim <jmdmk9st@naver.com>
2026-02-26 14:34:40 +0900

    [Gold III] Title: Dance Dance Revolution, Time: 328 ms, Memory: 121552 KB -BaekjoonHub

--- 백준/Gold/2342. Dance Dance Revolution/Dance Dance Revolution.py
+++ 백준/Gold/2342. Dance Dance Revolution/Dance Dance Revolution.py
@@ -3,13 +3,13 @@ input = sys.stdin.readline
 
 num_lst = list(map(int, input().split()))
 
-# dp 상태 정의 dp[i][l][r] : i 번째 지시까지 처리했을 때, 왼발이 l, 오른발이 r에 있는 상태로 만드는 최소 누적 비용
+# dp 상태 정의 dp[l][r] : 지금까지의 입력들을 모두 처리한 뒤, 왼발이 l, 오른발이 r에 있는 상태로 만드는 최소 누적 비용
 INF = float('inf')
 
 length = len(num_lst)
 
+# inf 는 아직 도달 못 한 부분
 dp = [[INF] * 5 for _ in range(5)]
-
 dp[0][0] = 0
 
 
@@ -26,13 +26,19 @@ def move(fr, to):
 
 for x in range(length - 1):
     num = num_lst[x]
+
+    # 새로운 상태를 담는 부분
     nxt = [[INF] * 5 for _ in range(5)]
 
     for l in range(5):
         for r in range(5):
+            if dp[l][r] == INF:
+                continue
+            # 같은 새 상태 (a, b) 에 도달하는 경로가 여러 개일 수 있음
             nxt[num][r] = min(nxt[num][r], dp[l][r] + move(l, num))
             nxt[l][num] = min(nxt[l][num], dp[l][r] + move(r, num))
 
     dp = nxt
 
+# 총 최소 비용 구하기
 sys.stdout.write(str(min(map(min, dp))))
