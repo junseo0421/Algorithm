7b45ab4e1b1584f8a0a7ddf160605a3172989f83
Junseo Kim <jmdmk9st@naver.com>
2026-04-03 20:10:39 +0900

    [Silver V] Title: 올림픽, Time: 40 ms, Memory: 33432 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -6,13 +6,16 @@ N, K = map(int, input().split())
 lst = [list(map(int, input().split())) for _ in range(N)]
 lst = sorted(lst, key = lambda x: (x[1], x[2], x[3]), reverse=True)
 
+if K == lst[0][0]:
+    print(1)
+    exit()
 
 lst[0].append(1)
 
 prev = lst[0]
 
-for i in range(1, N+1):
-    if prev[1:4] == lst[i][1:]:
+for i in range(1, N):
+    if prev[1:4] == lst[i][1:4]:
         lst[i].append(prev[4])
         prev = lst[i]
     else:
