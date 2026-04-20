da21d2eba7f9a263b578c7622d3118540df7fa3e
Junseo Kim <jmdmk9st@naver.com>
2026-04-21 03:04:28 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 684 ms, Memory: 233932 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -3,8 +3,6 @@ input = sys.stdin.readline
 
 from collections import deque
 
-INF = 1e9
-
 N, M = map(int, input().split())
 matrix = [list(map(int, input().rstrip())) for _ in range(N)]
 depth = [[[-1]*M for _ in range(N)] for _ in range(2)]
@@ -13,12 +11,15 @@ dx = [0, 0, 1, -1]
 dy = [1, -1, 0, 0]
 
 q = deque()
-q.append((0, 0, 0))
+q.append((0, 0, 0))  # x, y, crash(0: 아직 안 부숨, 1: 이미 부숨)
 depth[0][0][0] = 1
-depth[1][0][0] = 1
 
 while q:
     x, y, crash = q.popleft()
+
+    if x == N - 1 and y == M - 1:
+        sys.stdout.write(str(depth[crash][x][y]))
+        exit()
 
     for i in range(4):
         nx = x + dx[i]
@@ -27,21 +28,14 @@ while q:
         if not (0 <= nx < N and 0 <= ny < M):
             continue
 
-        if depth[crash][nx][ny] != -1:
-            continue
-
-        if matrix[nx][ny] == 0:
+        # 빈 칸으로 이동
+        if matrix[nx][ny] == 0 and depth[crash][nx][ny] == -1:
             depth[crash][nx][ny] = depth[crash][x][y] + 1
             q.append((nx, ny, crash))
-        elif matrix[nx][ny] == 1 and crash == 0:
+
+        # 벽을 아직 안 부쉈고, 이번에 벽을 부수는 경우
+        elif matrix[nx][ny] == 1 and crash == 0 and depth[1][nx][ny] == -1:
             depth[1][nx][ny] = depth[crash][x][y] + 1
             q.append((nx, ny, 1))
 
-if depth[0][N-1][M-1] == -1 and depth[1][N-1][M-1] == -1:
-    sys.stdout.write(str(-1))
-elif depth[0][N-1][M-1] == -1:
-    sys.stdout.write(str(depth[1][N-1][M-1]))
-elif depth[1][N-1][M-1] == -1:
-    sys.stdout.write(str(depth[0][N-1][M-1]))
-else:
-    sys.stdout.write(str(min(depth[0][N-1][M-1], depth[1][N-1][M-1])))
+sys.stdout.write(str(-1))
