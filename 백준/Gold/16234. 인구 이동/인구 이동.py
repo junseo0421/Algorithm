57ef618de83e1f90c1650792212d27de648e0cc7
Junseo Kim <jmdmk9st@naver.com>
2026-04-21 20:07:51 +0900

    [Gold IV] Title: 인구 이동, Time: 360 ms, Memory: 35644 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.py
+++ 백준/Gold/16234. 인구 이동/인구 이동.py
@@ -1,3 +1,5 @@
+# 최적화 버전
+
 import sys
 input = sys.stdin.readline
 
@@ -17,62 +19,67 @@ neighbor = {
     for y in range(N)
 }
 
-result = 0
+day = 0
+
+# 체크판 식으로 초기 시작점을 절반 정도로 줄임
+queue = [(i, j) for i in range(N) for j in range(i%2, N, 2)]
+
+# visited를 day 값으로 관리
+visited = [[-1] * N for _ in range(N)]
 
 while True:
-    # visited 2차원 배열은 while 문 안에 있어야함
-    visited = [[0] * N for _ in range(N)]
-
-    # 마지막에 평균값 넣어줄 때 사용할 list
+    # 마지막에 평균값 넣어줄 때 사용할 dict
     avg_dict = {}
 
-    # 모든 칸 순회
-    for i in range(N):
-        for j in range(N):
-            if visited[i][j] == 1:
-                continue
+    # 다음 날 탐색 시작점
+    next_queue = []
 
-            # bfs 적용
-            q = deque([(i, j)])
-            summation = 0
-            cnt = 0
-            union = []
+    # 이번 날에 필요한 칸만 순회
+    for i, j in queue:
+        if visited[i][j] == day:
+            continue
 
-            while q:
-                x, y = q.popleft()
+        # bfs 적용
+        q = deque([(i, j)])
+        union = [(i, j)]
+        visited[i][j] = day
 
-                if visited[x][y] == 1:
+        summation = maps[i][j]
+        cnt = 1
+
+        while q:
+            x, y = q.popleft()
+
+            for nx, ny in neighbor[(x, y)]:
+                if visited[nx][ny] == day:
                     continue
 
-                # 큐에 넣으면서 같은 연합인 부분의 좌표는 미리 다 저장해두기 (합도 미리 저장)
-                union.append((x, y))
+                # 차이의 절댓값이 L과 R 사이인 칸 큐에 넣기
+                if L <= abs(maps[x][y] - maps[nx][ny]) <= R:
+                    visited[nx][ny] = day
+                    q.append((nx, ny))
+                    union.append((nx, ny))
+                    summation += maps[nx][ny]
+                    cnt += 1
 
-                visited[x][y] = 1
-                summation += maps[x][y]
-                cnt += 1
+        if cnt == 1:
+            continue
 
-                for nx, ny in neighbor[(x, y)]:
-                    if visited[nx][ny] == 1:
-                        continue
+        avg = summation // cnt
 
-                    # 차이의 절댓값이 L과 R 사이인 칸 큐에 넣기
-                    if L <= abs(maps[x][y] - maps[nx][ny]) <= R:
-                        q.append((nx, ny))
+        for pos in union:
+            avg_dict[pos] = avg
 
-            if cnt == 1:
-                continue
+        # 연합이 생긴 좌표들만 다음 날 시작점 후보로 넘김
+        next_queue.extend(union)
 
-            avg = summation // cnt
-
-            for pos in union:
-                avg_dict[pos] = avg
-
-    if not avg_dict:
-        sys.stdout.write(str(result))
+    if not next_queue:
+        sys.stdout.write(str(day))
         break
 
     # 탐색이 '모두' 끝난 후, 미리 저장해둔 좌표에 평균 인구 넣기
     for position, av in avg_dict.items():
         maps[position[0]][position[1]] = av
 
-    result += 1
+    queue = next_queue
+    day += 1
