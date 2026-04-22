3056015c23bcf623cfb2498684150d99d9ec8cd8
Junseo Kim <jmdmk9st@naver.com>
2026-04-22 19:38:01 +0900

    [Gold V] Title: 토마토, Time: 632 ms, Memory: 216100 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -3,62 +3,46 @@ input = sys.stdin.readline
 
 from collections import deque
 
-M, N, H = map(int, input().split())
+M, N, H = map(int, input().split())  # 가로, 세로, 높이
+box = []
+q = deque()
 
-padding = M+2
+for h in range(H):
+    layer = []
+    for n in range(N):
+        row = list(map(int, input().split()))
+        layer.append(row)
+        for m in range(M):
+            if row[m] == 1:
+                q.append((h, n, m))  # 익은 토마토만 큐에 넣기
+    box.append(layer)
 
-layer_stride = padding * (N+2)
+# 위, 아래, 앞, 뒤, 왼쪽, 오른쪽
+dh = [1, -1, 0, 0, 0, 0]
+dn = [0, 0, 1, -1, 0, 0]
+dm = [0, 0, 0, 0, 1, -1]
 
-tomato = [-1] * layer_stride
+while q:
+    h, n, m = q.popleft()
 
-for _ in range(H):
-    tomato += [-1] * padding
+    for i in range(6):
+        nh = h + dh[i]
+        nn = n + dn[i]
+        nm = m + dm[i]
 
-    for _ in range(N):
-        tomato += [-1] + list(map(int, input().split())) + [-1]
+        if 0 <= nh < H and 0 <= nn < N and 0 <= nm < M:
+            if box[nh][nn][nm] == 0:
+                box[nh][nn][nm] = box[h][n][m] + 1
+                q.append((nh, nn, nm))
 
-    tomato += [-1] * padding
+answer = 0
 
-tomato += [-1] * layer_stride
+for h in range(H):
+    for n in range(N):
+        for m in range(M):
+            if box[h][n][m] == 0:
+                print(-1)
+                exit()
+            answer = max(answer, box[h][n][m])
 
-def bfs(x):  # x는 tomato가 1인 곳의 index lst
-    q = deque(x)
-
-    while q:
-        a = q.popleft()
-
-        for num in (a-1, a+1, a+padding, a-padding, a+layer_stride, a-layer_stride):
-            if tomato[num] == 0:
-                tomato[num] = tomato[a] + 1
-                q.append(num)
-
-one_lst = []
-
-for idx, i in enumerate(tomato):
-    if i == 1:
-        one_lst.append(idx)
-
-bfs(one_lst)
-
-orig_tomato = []
-
-for z in range(1, H+1):
-    for r in range(1, N+1):
-        for c in range(1, M+1):
-            idx = z*layer_stride + r*padding + c
-            orig_tomato.append(tomato[idx])
-
-if len(one_lst) == 0 and 0 not in orig_tomato:
-    sys.stdout.write('0')
-    exit()
-
-max_num = 1
-
-for num in orig_tomato:
-    if num == 0:
-        sys.stdout.write('-1')
-        exit()
-
-    max_num = max(max_num, num)
-
-sys.stdout.write(str(max_num - 1))
+print(answer - 1)
