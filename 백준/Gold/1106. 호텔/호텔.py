85d6ac44c3552221e9ea43fc72f25e778659355a
Junseo Kim <jmdmk9st@naver.com>
2026-04-22 22:45:46 +0900

    [Gold IV] Title: 호텔, Time: 4568 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/1106. 호텔/호텔.py
+++ 백준/Gold/1106. 호텔/호텔.py
@@ -2,20 +2,27 @@ import sys
 input = sys.stdin.readline
 
 C, N = map(int, input().split())
-INF = float('inf')
 
-# 비용으로 얻을 수 잇는 고객의 수가 100보다 작거나 같음
-# C=1 일 때, 비용 1에 대한 인원수가 100 이라면, dp[100] = 1
-dp = [INF] * (C+100)
-dp[0] = 0  # 0명일 때는 비용이 0
-
-# dp의 index는 고객의 수
-# 해당 index에 대한 값 (비용)
+hotel = [(0, 0)]
 
 for _ in range(N):
-    cost, benefit = map(int, input().split())
+    hotel.append(tuple(map(int, input().split())))
 
-    for i in range(benefit, C+100):
-        dp[i] = min(dp[i], dp[i-benefit]+cost)
+INF = float('inf')
+dp = [[INF] * (C + 1) for _ in range(N + 1)]
 
-sys.stdout.write(str(min(dp[C:])))
+for i in range(1, N + 1):
+    cost, customer = hotel[i][0], hotel[i][1]
+
+    for j in range(1, C + 1):
+        dp[i][j] = dp[i-1][j]  # 해당 호텔을 포함하지 않았을 경우
+
+        k = 0
+
+        while j - k * customer > 0:  # 최소 손님 수가 채워지지 않았을 경우
+            dp[i][j] = min(dp[i][j], dp[i-1][j - k * customer] + k * cost)
+            k += 1
+
+        dp[i][j] = min(dp[i][j], k * cost)
+
+print(dp[N][C])
