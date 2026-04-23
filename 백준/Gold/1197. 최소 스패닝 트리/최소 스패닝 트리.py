fb39db18e1ee0e7cb32aa543f223752d579775f8
Junseo Kim <jmdmk9st@naver.com>
2026-04-23 19:56:05 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 248 ms, Memory: 49576 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,50 +1,47 @@
 import sys
 input = sys.stdin.readline
 
-sys.setrecursionlimit(10**6)
-
-# Kruskal 알고리즘
-
-# 그리디 알고리즘의 일종. 사이클이 만들어지면 안되고, 간선은 n-1 개만 존재해야함.
-
-def find_parent(parent, x):
+def find_parent(x):
     if parent[x] != x:
-        parent[x] = find_parent(parent, parent[x])  # 경로 압축
+        parent[x] = find_parent(parent[x])
+
     return parent[x]
 
-def union_parent(parent, a, b):
-    a = find_parent(parent, a)
-    b = find_parent(parent, b)
+def union(a, b):
+    a = find_parent(a)
+    b = find_parent(b)
 
-    # 두 집합을 합침 (연결)
     if a < b:
-        parent[b] = a
-    else:
         parent[a] = b
-
-
+    else:
+        parent[b] = a
+        
+        
 V, E = map(int, input().split())
 
-parent = [0] * (V + 1)
 edges = []
 
-for i in range(1, V+1):  # parent 배열 초기화
-    parent[i] = i
-
 for _ in range(E):
-    A, B, weight = map(int, input().split())
-    edges.append((weight, A, B))
+    A, B, C = map(int, input().split())
+    edges.append((C, A, B))
 
-edges.sort()  # 가중치를 기준으로 오름차순으로 정렬
+edges.sort()
 
-result = 0
+parent = []
 
-for edge in edges:
-    w, a, b = edge
+for i in range(V + 1):
+    parent.append(i)
 
-    # 같은 집합에 포함이 안 됐을 경우 (사이클이 안 만들어지는 경우) 집합에 포함
-    if find_parent(parent, a) != find_parent(parent, b):
-        union_parent(parent, a, b)
-        result += w
+ans = 0
+used_edges = 0
 
-sys.stdout.write(str(result))
+for cost, start, finish in edges:
+    if find_parent(start) != find_parent(finish):
+        ans += cost
+        union(start, finish)
+        used_edges += 1
+
+        if used_edges == V - 1:
+            break
+
+print(ans)
